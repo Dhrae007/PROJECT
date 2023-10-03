4a8ab4fc3c1f09987ef68c771e90445a834ffd5f
unknown <fatundedamilare@gmail.com>
2023-10-03 19:22:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var string = "string";
+var number = 21;
+var object = ["moses","terzungwe"];
+var array = {string:"string", number:17};
+var boolean = true;
+
+var moses = "moses is a male";
+var drey =  6;
+var peter = ["peter is a boy","peter is an intelligent boy"];
+var solomon = {moses:"moses is twenty four years old",drey:24}
+var collins = true;
